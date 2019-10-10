a5bbb8d7dc6d9be8f839393e099470836ad2a3f2
nasri01 <root@localhost.localdomain>
2019-10-10 15:36:16 +0330

    l

--- ww/settings.py
+++ ww/settings.py
@@ -102,8 +102,8 @@ WSGI_APPLICATION = 'ww.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'kaadco.ir',
-        'NAME': 'kaadcoir_test',
+        'HOST': 'localhost',
+        'NAME': 'kaadcoir_azma',
         'PASSWORD': 'L@!Go&H^?f@a',
         'PORT': '5432',
         'USER': 'kaadcoir_admin',
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'ww/static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ww/static')
