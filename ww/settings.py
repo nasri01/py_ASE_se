74dcbf8836186ac23d186c2435e62d0d86d1df05
nasri01 <root@localhost.localdomain>
2019-10-10 19:16:28 +0330

    ew

--- ww/settings.py
+++ ww/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qhh(ydahm&a&(z45^jpsk2v@r7q2h_-@furoe@&md=s#f5)-c&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['192.168.1.5','0.0.0.0','10.42.0.1','127.0.0.1','192.168.1.104']
 ALLOWED_HOSTS = ['*']
