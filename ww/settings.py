0063b64dbf283a13487cedd781801685a1076ff9
nasri01 <root@localhost.localdomain>
2019-10-10 15:30:45 +0330

    fua

--- ww/settings.py
+++ ww/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'kaadco.ir',
-        'NAME': 'kaadcoir_azma',
+        'NAME': 'kaadcoir_test',
         'PASSWORD': 'L@!Go&H^?f@a',
         'PORT': '5432',
         'USER': 'kaadcoir_admin',
