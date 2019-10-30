498895fdcbb7254f7e96256b6eb195552b6fc100
Live session user <sdsa@sdas.sd>
2019-10-30 19:15:46 +0000

    sdad

--- form/models.py
+++ form/models.py
@@ -11,7 +11,7 @@ class cant_test(models.Model):
     date = jmodels.jDateTimeField()
     user = models.ForeignKey(User, on_delete=models.PROTECT) 
     record = models.ForeignKey(acc.models.record, on_delete=models.CASCADE, related_name='ctr')
-    totalcomment = models.ManyToManyField(acc.models.comment_cant_test, related_name='cttotalcomment')
+    totalcomment = models.ManyToManyField(acc.models.qualitive_comment_cant_test, related_name='cttotalcomment')
     is_done = models.BooleanField(default=False)
 
     
