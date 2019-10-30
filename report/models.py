5201d51e6f4640879c4b27ba5c6a22e774ad6e8e
Live session user <sdsa@sdas.sd>
2019-10-30 15:23:51 +0000

    sdsd

--- report/models.py
+++ report/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class report(models.Model):
+    tt = models.ForeignKey(acc.models.ad_test_type0, on_delete=models.PROTECT)
+    device = models.ForeignKey(acc.models.All_Device, on_delete=models.PROTECT)
+    request = models.ForeignKey(acc.models.Request, on_delete=models.PROTECT)
+    date = jmodels.jDateTimeField()
+    user = models.ForeignKey(User, on_delete=models.PROTECT) 
+    record = models.ForeignKey(acc.models.record, on_delete=models.CASCADE, related_name='ctr')
+    totalcomment = models.ManyToManyField(acc.models.comment_cant_test, related_name='cttotalcomment')
+    is_done = models.BooleanField(default=False)
