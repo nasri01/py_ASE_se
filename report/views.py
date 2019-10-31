ed7a121fc136069ace9ca80c4fc8e04d3b82d26c
Live session user <sdsa@sdas.sd>
2019-10-31 03:36:34 +0000

    sdsa

--- report/views.py
+++ report/views.py
@@ -514,7 +514,7 @@ def pdf(request):
                     css_root = static('/css')
                     css1 = CSS(filename=f'ww/{css_root}/sop2-pdf.css')
                     css2 = CSS(filename=f'ww/{css_root}/bootstrap-v4.min.css')
-                    HTML(string=html).write_pdf('reza.pdf',Presentational_hints=True,font_config=font_config, stylesheets=[css1, css2])
+                    HTML(string=html).write_pdf('reza.pdf',font_config=font_config, stylesheets=[css1, css2])
                     # f'{obj.request.number}/{obj.licence.number}.pdf'
                     
                     a12 = report.objects.create(tt = ad_test_type0.objects.get(name=modellist[s]),device = obj.device,
