0a45bce4510e5ff7a4c1dcab17b7a5cfef9946a8
youexampleco <you@example.co>
2019-11-02 11:04:40 +0000

    dasdas

--- report/views.py
+++ report/views.py
@@ -569,39 +569,15 @@ def pdf(request):
                         data2.append(int(modelobj[0].s2_e6_pr1.split('/')[1]))#47
                         data2.append(int(modelobj[0].s2_e6_pr2.split('/')[1]))#48
                         data2.append(int(modelobj[0].s2_e6_pr3.split('/')[1]))#49
-                        if (abs(int(modelobj[0].s1_e1_simp) - int(modelobj[0].s1_e1_nibpp)) >= 3):#6
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e2_simp) - int(modelobj[0].s1_e2_nibpp)) >= 3):#7
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e3_simp) - int(modelobj[0].s1_e3_nibpp)) >= 3.6):#8
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e4_simp) - int(modelobj[0].s1_e4_nibpp)) >= 6):#9
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e5_simp) - int(modelobj[0].s1_e5_nibpp)) >= 3):#10
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e6_simp) - int(modelobj[0].s1_e6_nibpp)) >= 3):#11
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e7_simp) - int(modelobj[0].s1_e7_nibpp)) >= 3.6):#12
-                            data3.append(0)
-                        else:
-                            data3.append(1)
-                        if (abs(int(modelobj[0].s1_e8_simp) - int(modelobj[0].s1_e8_nibpp)) >= 6):#13
-                            data3.append(0)
-                        else:
-                            data3.append(1)
                         
+                        data3.append(abs(int(modelobj[0].s1_e1_simp) - int(modelobj[0].s1_e1_nibpp))) #6    
+                        data3.append(abs(int(modelobj[0].s1_e2_simp) - int(modelobj[0].s1_e2_nibpp))) #7                        
+                        data3.append(abs(int(modelobj[0].s1_e3_simp) - int(modelobj[0].s1_e3_nibpp))) #8                        
+                        data3.append(abs(int(modelobj[0].s1_e4_simp) - int(modelobj[0].s1_e4_nibpp))) #9                        
+                        data3.append(abs(int(modelobj[0].s1_e5_simp) - int(modelobj[0].s1_e5_nibpp))) #10                        
+                        data3.append(abs(int(modelobj[0].s1_e6_simp) - int(modelobj[0].s1_e6_nibpp))) #11                       
+                        data3.append(abs(int(modelobj[0].s1_e7_simp) - int(modelobj[0].s1_e7_nibpp))) #12                       
+                        data3.append(abs(int(modelobj[0].s1_e8_simp) - int(modelobj[0].s1_e8_nibpp))) #13                       
                         
                         
                         data.append(sum(data1))#0
@@ -610,7 +586,6 @@ def pdf(request):
                         data.append(round(np.mean(data2),2))#3
                         data.append(round(np.std(data1),2))#4
                         data.append(round(np.std(data2),2))#5
-                        
                         data.append(data3)#6
                         data.append(data1)#7
                         data.append(data2)#8
