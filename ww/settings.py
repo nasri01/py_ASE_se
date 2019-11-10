cf86100136b34ce128f185e2d6914b97737913eb
you@example.co <you@example.co>
2019-11-10 10:21:44 +0330

    settings

--- ww/settings.py
+++ ww/settings.py
@@ -62,6 +62,7 @@ EMAIL_HOST_PASSWORD = '^=TYWtYv$&D0'
 
 
 MIDDLEWARE = [
+     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,14 +148,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR,'ww/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = '/home/kaadcoir/public_html/qc/static'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ww/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 
+try:
+    from local_settings import *
+except ImportError:
+    pass
+
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+DEBUG_PROPAGATE_EXCEPTIONS = True
