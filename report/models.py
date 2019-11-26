90d9b2e73258b7557ca01ad97de2f5b34b40ed2f
yoxample.com <you@example.co>
2019-11-26 07:11:02 +0330

    dsad

--- report/models.py
+++ report/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
 
 # Create your models here.
+class record(models.Model):
+    number= models.IntegerField(primary_key=True)
+    def __str__(self):
+        return str(self.number)
+class licence(models.Model):
+    number = models.IntegerField()
+    def __str__(self):
+        return  str(self.number)
 
 class report(models.Model):
     tt = models.ForeignKey(acc.models.ad_test_type0, on_delete=models.PROTECT)
@@ -11,8 +19,8 @@ class report(models.Model):
     request = models.ForeignKey(acc.models.Request, on_delete=models.PROTECT)
     date = jmodels.jDateTimeField()
     user = models.ForeignKey(User, on_delete=models.PROTECT) 
-    record = models.ForeignKey(acc.models.record, on_delete=models.CASCADE, related_name='rr')
-    licence = models.ForeignKey(acc.models.licence, on_delete=models.CASCADE, related_name='rl')
+    record = models.ForeignKey(record, on_delete=models.CASCADE, related_name='rr')
+    licence = models.ForeignKey(licence, on_delete=models.CASCADE, related_name='me1licence')
     status = models.ForeignKey(acc.models.ad_az_Status, on_delete=models.PROTECT)
     totalcomment = models.TextField(null=True, blank= True)
     is_done = models.BooleanField(default=False)
