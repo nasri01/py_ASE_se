4c951d23413b7d5b3ac777ceff91a6e9cce4d4af
Your Nam <you@example.co>
2020-01-05 18:05:45 +0330

    s

--- ww/settings.py
+++ ww/settings.py
@@ -62,6 +62,7 @@ EMAIL_HOST_PASSWORD = '^=TYWtYv$&D0'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,11 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'ww/static')
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 # STATIC_ROOT = '/home/kaadcoir/public_html/qc/static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'ww/static/'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
